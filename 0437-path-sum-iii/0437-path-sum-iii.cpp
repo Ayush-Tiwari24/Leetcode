3efb50ae63678a21404c8c26847b8546a6bf485e
Ayush Ranjan <ranjanayush2021@gmail.com>
2026-08-20 22:14:25 +0530

    Time: 10 ms (40.32%), Space: 19 MB (61.63%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void helper(TreeNode* root,long sum,int &count){
+        if(root==NULL)return;
+            if(root->val==sum){
+                count++;
+            }
+        helper(root->left,sum-root->val,count);
+        helper(root->right,sum-root->val,count);
+    }
+    int pathSum(TreeNode* root, int targetSum) {
+        if(root==NULL)return 0;
+        int count=0;
+        helper(root,targetSum,count);
+        count+=pathSum(root->left,targetSum);
+        count+=pathSum(root->right,targetSum);
+        return count;
+    }
+};
