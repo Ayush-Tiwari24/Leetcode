2a988652baa7602b3ca54e89690bd580174a6488
Ayush Ranjan <ranjanayush2021@gmail.com>
2026-09-09 23:58:54 +0530

    Time: 0 ms (100%), Space: 9.8 MB (25.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,17 +3,14 @@ public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length())return false;
         unordered_map<char,int>m1;
-        unordered_map<char,int>m2;
         for(int i=0;i<s.length();i++){
             m1[s[i]]++;
-            m2[t[i]]++;
         }
-        for(auto x:m1){
-            char ch1=x.first;
-            int freq1=x.second;
-            if(m2.find(ch1)!=m2.end()){
-                int freq2=m2[ch1];
-                if(freq1!=freq2)return false;
+        for(int i=0;i<t.length();i++){
+            char ch =t[i];
+            if(m1.find(ch)!=m1.end()){
+                m1[ch]--;
+                if(m1[ch]==0)m1.erase(ch);
             }
             else return false;
         }
