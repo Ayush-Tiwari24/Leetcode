b14dc224269c2980f2b431b2cf5021ce3c14c6fe
Ayush Ranjan <ranjanayush2021@gmail.com>
2026-09-11 18:26:01 +0530

    Time: 7 ms (57.74%), Space: 12.9 MB (72.43%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& arr) {
         vector<int> ans;
-        vector<int> freq(10, 0);
+        unordered_map<int,int>m;
 
         for (int x : arr)
-            freq[x]++;
+            m[x]++;
 
-        for (int i = 100; i <= 999; i++) {
+        for (int i = 100; i <= 999; i=i+2) {
             int x = i;
 
             int a = x % 10;
@@ -17,30 +17,18 @@ public:
             x /= 10;
 
             int c = x;
-
-            if (a % 2 != 0)
-                continue;
-
-            freq[c]--;
-
-            if (freq[c] >= 0) {
-                freq[b]--;
-
-                if (freq[b] >= 0) {
-                    freq[a]--;
-
-                    if (freq[a] >= 0)
-                        ans.push_back(i);
-
-                    freq[a]++;
+            if(m.find(a)!=m.end()){
+                m[a]--;
+                if(m[a]==0)m.erase(a);
+                if(m.find(b)!=m.end()){
+                    m[b]--;
+                    if(m[b]==0)m.erase(b);
+                    if(m.find(c)!=m.end())ans.push_back(i);
+                    m[b]++;
                 }
-
-                freq[b]++;
+                m[a]++;
             }
-
-            freq[c]++;
         }
-
         return ans;
     }
 };
